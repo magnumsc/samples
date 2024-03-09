d158cba8144d2d672eb5d24ce5359ef18c03d054
magnumsc <rscanno@hotmail.com>
2024-03-09 19:32:47 -0300

    change project to gradle

---
+++ src/main/java/com.magnum/Main.java
@@ -0,0 +1,18 @@
+package com.magnum;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(new StackPane(), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
